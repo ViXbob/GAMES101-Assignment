c9a118631abd2be73d528965261041fde1e8401c
ViXbob <vixbob@outlook.com>
2022-01-27 15:34:29 +0800

    complete pa1 and bonus

--- PA1/main.cpp
+++ PA1/main.cpp
@@ -19,13 +19,32 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     return view;
 }
 
-Eigen::Matrix4f get_model_matrix(float rotation_angle)
+Eigen::Matrix4f get_model_matrix(float rotation_angle, Eigen::Vector3f n = Eigen::Vector3f(0, 0, 1))
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
     // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
+
+    float alpha = rotation_angle / 180.0 * MY_PI;
+
+    Eigen::Matrix3f dual_matrix = Eigen::Matrix3f::Identity();
+    dual_matrix << 0, -n.z(), n.y(),
+                   n.z(), 0, -n.x(),
+                   -n.y(), n.x(), 0;
+    Eigen::Matrix3f model_3 = Eigen::Matrix3f::Identity();
+    model_3 = model_3 * cos(alpha) + (1 - cos(alpha)) * n * n.transpose() + sin(alpha) * dual_matrix;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            model(i, j) = model_3(i, j);
+        }
+    }
+    // std::cout << model << std::endl;
+    // model << cos(alpha), -sin(alpha), 0, 0,
+    //         sin(alpha), cos(alpha), 0, 0,
+    //         0, 0, 1, 0,
+    //         0, 0, 0, 1;
 
     return model;
 }
@@ -41,6 +60,27 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     // Create the projection matrix for the given parameters.
     // Then return it.
 
+    float alpha = eye_fov / 180.0 * MY_PI;
+    float t = fabs(zNear) * tan(alpha / 2.0);
+    float r = t * aspect_ratio;
+    float b = -t, l = -r, n = zNear, f = zFar;
+    Eigen::Matrix4f orthogonal = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f translate = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f scale = Eigen::Matrix4f::Identity();
+    scale << 2 / (r - l), 0, 0, 0,
+             0, 2 / (t - b), 0, 0,
+             0, 0, 2 / (n - f), 0,
+             0, 0, 0, 1;
+    translate << 1, 0, 0, -(l + r) / 2,
+                 0, 1, 0, -(b + t) / 2,
+                 0, 0, 1, -(n + f) / 2,
+                 0, 0, 0, 1;
+    orthogonal = scale * translate;
+    projection << n, 0, 0, 0,
+                  0, n, 0, 0,
+                  0, 0, n + f, -n * f,
+                  0, 0, 1, 0;
+    projection = orthogonal * projection;
     return projection;
 }
 
@@ -75,9 +115,9 @@ int main(int argc, const char** argv)
     if (command_line) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
-        r.set_model(get_model_matrix(angle));
+        r.set_model(get_model_matrix(angle, Eigen::Vector3f(0, 0, 1)));
         r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+        r.set_projection(get_projection_matrix(45, 1, -0.1, -50));
 
         r.draw(pos_id, ind_id, rst::Primitive::Triangle);
         cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
@@ -93,7 +133,7 @@ int main(int argc, const char** argv)
 
         r.set_model(get_model_matrix(angle));
         r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+        r.set_projection(get_projection_matrix(45, 1, -0.1, -50));
 
         r.draw(pos_id, ind_id, rst::Primitive::Triangle);
 
