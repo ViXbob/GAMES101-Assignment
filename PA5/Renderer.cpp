77a36ee423fa05dd114bf9557b26e370ea9e1434
ViXbob <vixbob@outlook.com>
2022-02-05 01:02:03 +0800

    complete pa5

--- PA5/Renderer.cpp
+++ PA5/Renderer.cpp
@@ -229,12 +229,16 @@ void Renderer::Render(const Scene& scene)
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
             // x (horizontal) variable with the *imageAspectRatio*            
-
+            float n = scene.height / (scale * 2.0);
+            x = (i - scene.width / 2.0) / n;
+            y = (-j + scene.height / 2.0) / n;
             Vector3f dir = Vector3f(x, y, -1); // Don't forget to normalize this direction!
+            dir = normalize(dir);
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
         }
         UpdateProgress(j / (float)scene.height);
     }
+    std::cout << std::endl;
 
     // save framebuffer to file
     FILE* fp = fopen("binary.ppm", "wb");
